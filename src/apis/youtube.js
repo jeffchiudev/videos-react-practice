853e4f9ae7a0ffbeba1d8705bf52fc886e9ce9c8
Jeff Chiu <jwchiu82@gmail.com>
2021-08-09 21:41:08 -0700

    able to make fetch request to youtube api

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,13 @@
+import axios from "axios";
+
 const KEY = "AIzaSyDKphFUSRV0yi_7ngs0rKmA005HQ44KTW4";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    type: "video",
+    maxResults: 5,
+    key: KEY,
+  },
+});
